bc2b5f1b6f52818b78c9c7184b94e8e9cafc59ad
Even <1392234761@qq.com>
2020-03-02 17:58:27 +0800

    个股贡献度模块

--- src/views/ind-contribution/ContributionTable.vue
+++ src/views/ind-contribution/ContributionTable.vue
@@ -1,26 +1,42 @@
 <template>
   <div>
-    <div style="min-height: 100px;" >
-      <DateSelect @dateSelectChange="dateChange"></DateSelect>
+    <div style="margin-left: 20px;margin-top: 10px;clear: both">
+      <div id="select_div" style="">
+        <div id="select_date" style="margin-right: 100px;float: left">
+          <DateSelect @dateSelectChange="dateChange"></DateSelect>
+        </div>
+        <div id="select_index">
+          <el-select v-model="curIndex" style="width: 100px" placeholder="请选择">
+            <el-option
+              v-for="item in indexes"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+      </div>
       <p></p>
       <el-button @click="refresh">刷新</el-button>
     </div>
-
-    <div >
+    <p></p>
+    <div style="text-align: center;background-color: #587482;margin-left: 10px;margin-right: 10px">
       <v-table
         is-horizontal-resize
         column-width-drag
-        multiple-sort
-        :is-loading="isLoading"
+        :multiple-sort="multipleSort"
         style="width:100%"
         :columns="tableConfig.columns"
         :table-data="tableConfig.tableData"
+        :is-loading="isLoading"
         @sort-change="sortChange"
         row-hover-color="#eee"
         row-click-color="#edf7ff"
-        :paging-index="(pageIndex-1)*pageSize"
+        :paging-index="(pageIndex - 1) * pageSize"
+        odd-bg-color="#78909C"
+        even-bg-color="#90979c"
       ></v-table>
-      <div class="table_pagination_partition" style="height: 10px">  </div>
+      <p></p>
       <v-pagination
         @page-change="pageChange"
         @page-size-change="pageSizeChange"
@@ -34,6 +50,7 @@
 
 <script>
   import DateSelect from "./DateSelect";
+
   export default {
     name: "ContributionTable",
     components: {
@@ -41,24 +58,32 @@
     },
     data() {
       return {
-        date:"",
-        rawData:[],
-        pageIndex:1,
-        pageSize:20,
-        isLoading:false,
-        total:0,
+        date: "",
+        indexes: [{value: 'SSE', label: '上证'}, {value: 'SZSE', label: '深证'}],
+        curIndex: 'SSE',
+        rawData: [],
+        pageIndex: 1,
+        pageSize: 20,
+        isLoading: false,
+        total: 0,
+        multipleSort: false,
         tableConfig: {
           tableData: [],
           columns: [
             {
               field: 'order', title: 'TOP', width: 50, titleAlign: 'center', columnAlign: 'center',
-              formatter: function (rowData,index,pagingIndex) {
-                var currentIndex=index+pagingIndex;
-                return currentIndex < 10 ? '<span style="color:red;font-weight: bold;">' + (currentIndex + 1) + '</span>' : currentIndex + 1
+              formatter: function (rowData, index, pagingIndex) {
+                var currentIndex = index + pagingIndex;
+                return currentIndex < 10 ? '<span style="color:red;font-weight: bold;">' + (currentIndex + 1) + '</span>' : currentIndex + 1;
               }, isFrozen: true
             },
             {field: 'symbol', title: '股票代码', width: 80, titleAlign: 'center', columnAlign: 'center', isResize: true},
-            {field: 'name', title: '股票名', width: 150, titleAlign: 'center', columnAlign: 'center', isResize: true},
+            {
+              field: 'name', title: '股票名', width: 150, titleAlign: 'center', columnAlign: 'center', isResize: true,
+              formatter: function (rowData, rowIndex, pagingIndex, field) {
+                return '<span style="color:orange;font-weight: bold;">' + (rowData[field]) + '</span>';
+              }, isFrozen: true
+            },
             {
               field: 'pctChg',
               title: '涨跌幅(%)',
@@ -85,7 +110,7 @@
               isResize: true,
               orderBy: 'asc',
               formatter: function (rowData, rowIndex, pagingIndex, field) {
-                let num = rowData[field] * Math.pow(10,9);
+                let num = rowData[field] * Math.pow(10, 9);
                 return num.toFixed(2);
               }
             },
@@ -98,7 +123,7 @@
               isResize: true,
               orderBy: 'asc',
               formatter: function (rowData, rowIndex, pagingIndex, field) {
-                let num = rowData[field] * Math.pow(10,9);
+                let num = rowData[field] * Math.pow(10, 9);
                 return num.toFixed(2);
               }
             }
@@ -109,9 +134,9 @@
     async created() {
       let myDate = new Date;
       let year = myDate.getFullYear();//获取当前年
-      let month = myDate.getMonth()+1;//获取当前月
+      let month = myDate.getMonth() + 1;//获取当前月
       let day = myDate.getDate();//获取当前日
-      let dateObj={
+      let dateObj = {
         year: year,
         month: month,
         day: day
@@ -120,13 +145,15 @@
       await this.getRawData(this.date);
       await this.getTableData();
     },
-    methods:{
-      async getRawData(date){
+    methods: {
+      async getRawData(date) {
         //使用axios的get请求向后台获取用户信息数据
+        this.isLoading = true;
         try {
           let ret = await this.$axios.get('/contribution', {
               params: {
-                date: date
+                date: date,
+                index: this.curIndex
               }
             }
           );
@@ -145,63 +172,82 @@
             this.rawData.push(websit);
           }
           this.total = this.rawData.length;
-        }catch(err){
+        } catch (err) {
           console.log(err);
           alert("请求出错！");
         }
-      },
-      // 请求等待控制
-      request(){
-        this.isLoading=true;
-        setTimeout(x=>{
-          this.isLoading=false;
-        },20000);
+        this.isLoading = false;
       },
       // 获取 table 组件每次操作后的参数（重新去请求数据）
-      sortChange(params){
-        console.log(params);
-        if (params.height.length > 0){
+      sortChange(params) {
+        let orderColumn = 'pctChg';
+        let orderBy = '';
+        if (params.pctChg.length > 0) {
+          orderBy = params.pctChg;
+        } else if (params.contribution.length > 0) {
+          orderColumn = 'contribution';
+          orderBy = params.contribution;
+        } else if (params.contribution5.length > 0) {
+          orderColumn = 'contribution5';
+          orderBy = params.contribution5;
+        }
+        console.log(orderColumn);
+        console.log(orderBy);
+        if (orderBy.length > 0) {
           this.tableConfig.tableData.sort(function (a, b) {
-            if (params.height === 'asc'){
-              return a.height - b.height;
-            }else if(params.height === 'desc'){
-              return b.height - a.height;
-            }else{
+            if (orderBy === 'asc') {
+              if (orderColumn === 'pctChg') {
+                console.log("jsgfsjhdgfhds");
+                return a.pctChg - b.pctChg;
+              } else if (orderColumn === 'contribution') {
+                return a.contribution - b.contribution;
+              } else if (orderColumn === 'contribution5') {
+                return a.contribution5 - b.contribution5;
+              }
+            } else if (orderBy === 'desc') {
+              if (orderColumn === 'pctChg') {
+                return b.pctChg - a.pctChg;
+              } else if (orderColumn === 'contribution') {
+                return b.contribution - a.contribution;
+              } else if (orderColumn === 'contribution5') {
+                return b.contribution5 - a.contribution5;
+              }
+            } else {
               return 0;
             }
           });
         }
       },
       //获取当前页数据
-      getTableData(){
-        this.tableConfig.tableData=this.rawData.slice((this.pageIndex-1) * this.pageSize,this.pageIndex * this.pageSize);
+      getTableData() {
+        this.tableConfig.tableData = this.rawData.slice((this.pageIndex - 1) * this.pageSize, this.pageIndex * this.pageSize);
       },
-      pageChange(pageIndex){
+      pageChange(pageIndex) {
         this.pageIndex = pageIndex;
         this.getTableData();
       },
-      pageSizeChange(pageSize){
+      pageSizeChange(pageSize) {
         this.pageIndex = 1;
         this.pageSize = pageSize;
         this.getTableData();
       },
-      async refresh(){
-        this.rawData=[];  //重置数据
+      async refresh() {
+        this.rawData = [];  //重置数据
         await this.getRawData(this.date);
         await this.getTableData();
       },
-      dateChange(obj){
+      dateChange(obj) {
         this.date = this.dateObjToStr(obj);
       },
-      dateObjToStr(obj){
+      dateObjToStr(obj) {
         var year = obj.year;
         var month = obj.month;
         var day = obj.day;
 
-        if(month<10){
+        if (month < 10) {
           month = "0" + month;
         }
-        if(day<10){
+        if (day < 10) {
           day = "0" + day;
         }
 
@@ -212,15 +258,16 @@
 </script>
 
 <style scoped>
-  .layout{
+  .layout {
     border: 1px solid #d7dde4;
-    background: #f5f7f9;
+    background: #90979c;
     position: relative;
     border-radius: 4px;
     overflow: hidden;
     height: 100%;
   }
-  .layout-logo{
+
+  .layout-logo {
     width: 100px;
     height: 30px;
     background: #5b6270;
@@ -233,12 +280,15 @@
     text-align: center;
     color: #49ffcc;
   }
-  .layout-nav{
+
+  .layout-nav {
     width: 420px;
     margin: 0 auto;
     margin-right: 20px;
   }
-  .layout-footer-center{
+
+  .layout-footer-center {
     text-align: center;
   }
+
 </style>
--- src/views/ind-contribution/DateSelect.vue
+++ src/views/ind-contribution/DateSelect.vue
@@ -1,7 +1,7 @@
 <!-- 日期搜索框 -->
 <template>
-  <div id = "myDate">
-    <el-button icon="el-icon-caret-left"  @click="dateChange(4)" ></el-button>
+  <div id="myDate">
+    <el-button icon="el-icon-caret-left" @click="dateChange(4)"></el-button>
     <el-select v-model="yearsModel" @change="dateChange(1)" placeholder="请选择" style="width:100px;">
       <el-option
         v-for="item in years"
@@ -26,23 +26,22 @@
         :value="item.value">
       </el-option>
     </el-select>
-    <el-button icon="el-icon-caret-right"   @click="dateChange(5)"  ></el-button>
+    <el-button icon="el-icon-caret-right" @click="dateChange(5)"></el-button>
   </div>
 </template>
 
 <script>
   export default {
     name: "DateSelect",
-    props: {
-    },
+    props: {},
     data() {
       return {
-        yearsModel:null,
-        years:[],
-        monthsModel:null,
-        months:[],
-        daysModel:null,
-        days:[],
+        yearsModel: null,
+        years: [],
+        monthsModel: null,
+        months: [],
+        daysModel: null,
+        days: [],
       }
     },
 
@@ -50,128 +49,129 @@
       this.init();
     },
     methods: {
-      init(){
+      init() {
         var myDate = new Date;
         var year = myDate.getFullYear();//获取当前年
-        var month = myDate.getMonth()+1;//获取当前月
+        var month = myDate.getMonth() + 1;//获取当前月
         var day = myDate.getDate();//获取当前日
 
         this.initSelectYear(year);
         this.initSelectMonth();
-        this.initSelectDay(year,month);
+        this.initSelectDay(year, month);
         this.yearsModel = year;
         this.monthsModel = month;
         this.daysModel = day;
 
         let obj = {
-          year:this.yearsModel,
-          month:this.monthsModel,
-          day:this.daysModel
+          year: this.yearsModel,
+          month: this.monthsModel,
+          day: this.daysModel
         };
-        this.$emit('dateSelectChange',obj);
+        this.$emit('dateSelectChange', obj);
 
       },
-      initSelectYear(year){
+      initSelectYear(year) {
         this.years = [];
-        for(let i=0;i<30;i++){
-          this.years.push({value:(year - i),label:(year - i)+ "年"});
+        for (let i = 0; i < 30; i++) {
+          this.years.push({value: (year - i), label: (year - i) + "年"});
         }
       },
-      initSelectMonth(){
+      initSelectMonth() {
         this.months = [];
-        this.months.push({value:0,label:"全部"});
-        for(let i=1;i<=12;i++){
-          this.months.push({value:i,label:i+ "月"});
+        this.months.push({value: 0, label: "全部"});
+        for (let i = 1; i <= 12; i++) {
+          this.months.push({value: i, label: i + "月"});
         }
       },
-      initSelectDay(year,month){
-        var maxDay = this.getMaxDay(year,month);
+      initSelectDay(year, month) {
+        var maxDay = this.getMaxDay(year, month);
         this.days = [];
-        this.days.push({value:0,label:"全部"});
-        for(var i=1;i<=maxDay;i++){
-          this.days.push({value:i,label:i+ "日"});
+        this.days.push({value: 0, label: "全部"});
+        for (var i = 1; i <= maxDay; i++) {
+          this.days.push({value: i, label: i + "日"});
         }
       },
-      dateChange(type){
+      dateChange(type) {
         //1年 2月 3日 4 左 5右
-        if(type == 1||type == 2){
-          if(this.monthsModel == 0){
+        if (type == 1 || type == 2) {
+          if (this.monthsModel == 0) {
             this.daysModel = 0;
-            this.initSelectDay(this.yearsModel,1);
-          }else{
-            this.initSelectDay(this.yearsModel,this.monthsModel);
+            this.initSelectDay(this.yearsModel, 1);
+          } else {
+            this.initSelectDay(this.yearsModel, this.monthsModel);
           }
-        }
-        else if(type == 4){
+        } else if (type == 4) {
           this.dayleft();
-        }
-        else if(type == 5){
+        } else if (type == 5) {
           this.dayright();
         }
-        //console.log(this.yearsModel);
-
 
         //操作父组件方法
-        let obj ={
-          year:this.yearsModel,
-          month:this.monthsModel,
-          day:this.daysModel
+        let obj = {
+          year: this.yearsModel,
+          month: this.monthsModel,
+          day: this.daysModel
         }
-        this.$emit('dateSelectChange',obj);
+        this.$emit('dateSelectChange', obj);
       },
-      dayleft(){
+      dayleft() {
         var tmpYear = this.yearsModel;
-        var tmpMonth =  this.monthsModel;
+        var tmpMonth = this.monthsModel;
         var tmpDay = this.daysModel;
-        if(tmpYear ==null){
+        if (tmpYear == null) {
           var myDate = new Date;
           var year = myDate.getFullYear();//获取当前年
-          var month = myDate.getMonth()+1;//获取当前月
+          var month = myDate.getMonth() + 1;//获取当前月
           var day = myDate.getDate();//获取当前日
           this.yearsModel = year;
           this.monthsModel = month;
           this.daysModel = day;
           return;
         }
-        if(tmpMonth ==null){ tmpMonth = 0}
-        if(tmpDay ==null){ tmpDay = 0}
+        if (tmpMonth == null) {
+          tmpMonth = 0
+        }
+        if (tmpDay == null) {
+          tmpDay = 0
+        }
 
         var yearV = tmpYear;
-        var monthV = tmpMonth ;
+        var monthV = tmpMonth;
         var dayV = tmpDay;
 
-        if((tmpMonth == 0||tmpMonth == 1) && (tmpDay == 0||tmpDay == 1)){
-          yearV = tmpYear  - 1; monthV = 12;
-          dayV = this.getMaxDay(tmpYear,tmpMonth);
+        if ((tmpMonth == 0 || tmpMonth == 1) && (tmpDay == 0 || tmpDay == 1)) {
+          yearV = tmpYear - 1;
+          monthV = 12;
+          dayV = this.getMaxDay(tmpYear, tmpMonth);
         }
-        if(!(tmpMonth == 0||tmpMonth == 1) && (tmpDay == 0||tmpDay == 1)){
+        if (!(tmpMonth == 0 || tmpMonth == 1) && (tmpDay == 0 || tmpDay == 1)) {
           monthV = tmpMonth - 1;
-          dayV = this.getMaxDay(tmpYear,tmpMonth);
+          dayV = this.getMaxDay(tmpYear, tmpMonth);
         }
-        if((tmpMonth == 0||tmpMonth == 1) && !(tmpDay == 0||tmpDay == 1)){
+        if ((tmpMonth == 0 || tmpMonth == 1) && !(tmpDay == 0 || tmpDay == 1)) {
           dayV = tmpDay - 1;
         }
-        if(!(tmpMonth == 0||tmpMonth == 1) && !(tmpDay == 0||tmpDay == 1)){
+        if (!(tmpMonth == 0 || tmpMonth == 1) && !(tmpDay == 0 || tmpDay == 1)) {
           dayV = tmpDay - 1;
         }
         this.yearsModel = yearV;
         this.monthsModel = monthV;
         this.daysModel = dayV;
       },
-      dayright(){
+      dayright() {
         var myDate = new Date;
         var year = myDate.getFullYear();//获取当前年
-        var month = myDate.getMonth()+1;//获取当前月
+        var month = myDate.getMonth() + 1;//获取当前月
         var day = myDate.getDate();//获取当前日
 
         var tmpYear = this.yearsModel;
-        var tmpMonth =  this.monthsModel;
+        var tmpMonth = this.monthsModel;
         var tmpDay = this.daysModel;
 
-        if(tmpYear ==null){
+        if (tmpYear == null) {
           var myDate = new Date;
           var year = myDate.getFullYear();//获取当前年
-          var month = myDate.getMonth()+1;//获取当前月
+          var month = myDate.getMonth() + 1;//获取当前月
           var day = myDate.getDate();//获取当前日
           this.yearsModel = year;
           this.monthsModel = month;
@@ -179,16 +179,20 @@
           return;
         }
 
-        if(tmpMonth ==null){ tmpMonth = 0}
-        if(tmpDay ==null){ tmpDay = 0}
-        if(tmpYear>year){
+        if (tmpMonth == null) {
+          tmpMonth = 0
+        }
+        if (tmpDay == null) {
+          tmpDay = 0
+        }
+        if (tmpYear > year) {
           this.yearsModel = year;
           this.monthsModel = month;
           this.daysModel = day;
           return;
         }
-        if(tmpYear == year){
-          if((tmpMonth > month || tmpMonth ==0 )||(tmpMonth == month )&&(tmpDay >= day || tmpDay ==0)){
+        if (tmpYear == year) {
+          if ((tmpMonth > month || tmpMonth == 0) || (tmpMonth == month) && (tmpDay >= day || tmpDay == 0)) {
             this.yearsModel = year;
             this.monthsModel = month;
             this.daysModel = day;
@@ -196,20 +200,21 @@
           }
         }
 
-        var maxDay = this.getMaxDay(tmpYear,tmpMonth);
+        var maxDay = this.getMaxDay(tmpYear, tmpMonth);
         var yearV = tmpYear;
-        var monthV = tmpMonth ;
+        var monthV = tmpMonth;
         var dayV = tmpDay;
 
-        if((tmpMonth == 0 || tmpMonth == 12) && (tmpDay == 0 || tmpDay == maxDay)){
-          yearV = tmpYear  + 1; monthV = 1;
+        if ((tmpMonth == 0 || tmpMonth == 12) && (tmpDay == 0 || tmpDay == maxDay)) {
+          yearV = tmpYear + 1;
+          monthV = 1;
           dayV = 1;
         }
-        if(!(tmpMonth == 0 || tmpMonth == 12) && (tmpDay == 0 || tmpDay == maxDay)){
+        if (!(tmpMonth == 0 || tmpMonth == 12) && (tmpDay == 0 || tmpDay == maxDay)) {
           monthV = tmpMonth + 1;
           dayV = 1;
         }
-        if(!(tmpMonth == 0 || tmpMonth == 12) && !(tmpDay == 0 || tmpDay == maxDay)){
+        if (!(tmpMonth == 0 || tmpMonth == 12) && !(tmpDay == 0 || tmpDay == maxDay)) {
           dayV = tmpDay + 1;
         }
 
@@ -218,16 +223,16 @@
         this.daysModel = dayV;
 
       },
-      getMaxDay(year,month){
+      getMaxDay(year, month) {
         var new_year = year;  //取当前的年份
         var new_month = month++;//取下一个月的第一天，方便计算（最后一天不固定）
-        if(month>12)      //如果当前大于12月，则年份转到下一年
+        if (month > 12)      //如果当前大于12月，则年份转到下一年
         {
-          new_month -=12;    //月份减
+          new_month -= 12;    //月份减
           new_year++;      //年份增
         }
-        var new_date = new Date(new_year,new_month,1);//取当年当月中的第一天
-        return (new Date(new_date.getTime()-1000*60*60*24)).getDate();//获取当月最后一天日期
+        var new_date = new Date(new_year, new_month, 1);//取当年当月中的第一天
+        return (new Date(new_date.getTime() - 1000 * 60 * 60 * 24)).getDate();//获取当月最后一天日期
       }
     }
   }
