a45ea3405f7a0e5202cdf505a03ddd20f417a3ef
TXueXue <43594603+TXueXue@users.noreply.github.com>
2020-04-28 17:25:55 +0800

    部署

--- src/main.js
+++ src/main.js
@@ -27,8 +27,8 @@ Vue.prototype.$echarts = echarts
 
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
-axios.defaults.baseURL = 'http://localhost:8443/api'
-// axios.defaults.baseURL = 'http://39.98.185.229:8443/api'
+// axios.defaults.baseURL = 'http://localhost:8443/api'
+axios.defaults.baseURL = 'http://39.98.185.229:8443/api'
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
